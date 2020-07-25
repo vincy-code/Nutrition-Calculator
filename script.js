95e98c28ad1039a0f0f4e9b47ff95144111a7cc0
Matthew Kerr <matt777k@gmail.com>
2020-07-25 09:36:57 -0400

    fdcAPI is now working; second link giving 401 unauthorized code

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-var fdcApi = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?limit=1&api_key=93FB5ZkYGDf50t9MdkGtFJZSj09FqLI9engO84mR";
+var fdcApi = "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=93FB5ZkYGDf50t9MdkGtFJZSj09FqLI9engO84mR&query=Cheddar%20Cheese";
 
 var nutritionixApi = "https://trackapi.nutritionix.com/v2/search/instant?detailed=true&query=spaghettibolognese&api_key=fca954f6d34563952bd28e3af019024a";
