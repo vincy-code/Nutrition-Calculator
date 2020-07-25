dc1819455a1f878fb652b4920fb766385a48c923
Matthew Kerr <matt777k@gmail.com>
2020-07-25 09:24:26 -0400

    added API keys to JS file

---
+++ script.js
@@ -0,0 +1,3 @@
+var fdcApi = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?limit=1&api_key=93FB5ZkYGDf50t9MdkGtFJZSj09FqLI9engO84mR";
+
+var nutritionixApi = "https://trackapi.nutritionix.com/v2/search/instant?detailed=true&query=spaghettibolognese&api_key=fca954f6d34563952bd28e3af019024a";
